0c02d34263247721a1b0b4ac7160cd63343adbc6
alegent <alegent@student.42.fr>
2015-06-02 14:42:43 +0200

    Diffilement presque entierement au point (plus qu'a modifier ft_escape and it's on !)

--- srcs/ft_left.c
+++ srcs/ft_left.c
@@ -6,7 +6,7 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/02 13:57:47 by alegent           #+#    #+#             */
-/*   Updated: 2015/06/02 14:00:48 by alegent          ###   ########.fr       */
+/*   Updated: 2015/06/02 14:41:01 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 int										ft_left(t_lst **node)
 {
+	int									len;
 	t_env								*env;
 
+	len = ft_lstlen();
 	env = ft_sglt();
 	(*node)->flag &= ~UNDER;
 	ft_update(*node);
 	*node = ((*node)->prev == env->arg) ? env->arg->prev : (*node)->prev;
 	(*node)->flag |= UNDER;
-	if ((*node)->pos->y - env->scroll >= env->screen->y)
+	if ((*node)->pos->y - env->scroll > env->screen->y)
 	{
-		env->scroll = (*node)->pos->y - env->screen->y;
-		env->scroll++;
+		env->scroll = len - env->screen->y;
 		ft_plst();
 	}
-	else if ((*node)->pos->y < env->screen->y + env->scroll
-			&& env->scroll > 0)
+	else if ((*node)->pos->y < env->scroll && env->scroll > 0)
 	{
 		env->scroll--;
 		ft_plst();
--- srcs/ft_up.c
+++ srcs/ft_up.c
@@ -6,7 +6,7 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/02 14:01:45 by alegent           #+#    #+#             */
-/*   Updated: 2015/06/02 14:05:42 by alegent          ###   ########.fr       */
+/*   Updated: 2015/06/02 14:41:06 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,23 @@
 
 int									ft_up(t_lst **node)
 {
+	int								len;
 	t_env							*env;
 
 	env = ft_sglt();
 	(*node)->flag &= ~UNDER;
 	ft_update(*node);
-	if (ft_lstlen() > 1)
+	if ((len = ft_lstlen()) > 1)
 		ft_findprev(env, node);
 	else
 		*node = ((*node)->prev == env->arg) ? env->arg->prev : (*node)->prev;
 	(*node)->flag |= UNDER;
-	if ((*node)->pos->y - env->scroll >= env->screen->y)
+	if ((*node)->pos->y - env->scroll > env->screen->y)
 	{
-		env->scroll = (*node)->pos->y - env->screen->y;
-		env->scroll++;
+		env->scroll = len - env->screen->y;
 		ft_plst();
 	}
-	else if ((*node)->pos->y < env->screen->y + env->scroll
-			&& env->scroll > 0)
+	else if ((*node)->pos->y < env->scroll && env->scroll > 0)
 	{
 		env->scroll--;
 		ft_plst();
