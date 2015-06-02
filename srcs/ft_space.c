2f2dc1dc91ac51a38e8dfe4982c372f4d9659217
alegent <alegent@student.42.fr>
2015-06-02 17:19:25 +0200

    Changement de ft_space afin d'y integrer le defilement de la page, juste au cas ou xD

--- srcs/ft_space.c
+++ srcs/ft_space.c
@@ -6,7 +6,7 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/01 20:42:31 by alegent           #+#    #+#             */
-/*   Updated: 2015/06/01 20:46:57 by alegent          ###   ########.fr       */
+/*   Updated: 2015/06/02 17:16:36 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,16 @@ void				ft_space(t_env *env, t_lst **node)
 	ft_update(*node);
 	*node = ((*node)->next == env->arg) ? env->arg->next : (*node)->next;
 	(*node)->flag |= UNDER;
-	ft_update(*node);
+	if ((*node)->pos->y - env->scroll >= env->screen->y)
+	{
+		env->scroll++;
+		ft_plst();
+	}
+	else if ((*node)->pos->y == 0)
+	{
+		env->scroll = 0;
+		ft_plst();
+	}
+	else
+		ft_update(*node);
 }
